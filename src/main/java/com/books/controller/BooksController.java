1d996b0e7c03356aaf0076421deb28c1edb21f2a
shreya.se <shreyase@zeta.tech>
2024-02-22 14:17:26 +0530

    Commit finished Buy

--- src/main/java/com/books/controller/BooksController.java
+++ src/main/java/com/books/controller/BooksController.java
@@ -17,30 +17,32 @@ public class BooksController {
     @Autowired
     UserController users;
 
-    @RequestMapping(value="/books",method= RequestMethod.GET)
-    public List<Books> getBooks(){
+    @RequestMapping(value = "/books", method = RequestMethod.GET)
+    public List<Books> getBooks() {
         return service.showBooks();
     }
 
-    @RequestMapping(value="/books/{book_name}", method=RequestMethod.GET)
-    public Books GetBookbyName(@PathVariable String book_name) throws Exception{
-        Optional<Books> book= service.getBookByName(book_name);
+    @RequestMapping(value = "/books/{book_name}", method = RequestMethod.GET)
+    public Books GetBookbyName(@PathVariable String book_name) throws Exception {
+        Optional<Books> book = service.getBookByName(book_name);
         return book.get();
     }
 
-    @RequestMapping(value="/book/add/{username}", method=RequestMethod.POST)
+    @RequestMapping(value = "/book/add/{username}", method = RequestMethod.POST)
     public Object AddBook(@PathVariable String username, @RequestBody Books book) throws Exception {
         Users user1 = users.GetUser(username);
+        Books oldbook = GetBookbyName(book.getBookname());
         if (Objects.equals(user1.getType(), "admin")) {
-            return service.addNewBook(book);
+            if (oldbook == null) {
+                return service.addNewBook(book);
+            } else {
+                service.updateInventory(oldbook.getInventory() + book.getInventory(), book.getBookname());
+                return GetBookbyName(oldbook.getBookname());
+            }
         } else {
             return ("You are not admin");
         }
     }
-
-
-
-
 
 
 }
